d9fa75d7c7c78a010754f88bed6e72dc0782601c
P4o1o <pchecchin.bp@gmail.com>
2024-12-12 16:00:12 +0100

    nasm version

--- siphash_nasm.asm
+++ siphash_nasm.asm
@@ -5,13 +5,10 @@ global siphash_4_8
 %macro InitMainValues 0
     mov r10, 0x0736f6d6570736575   ; r10 = v0
     xor r10, rdx                     ; r10 = v0 ^ key0
-    
     mov r11, 0x0646f72616e646f6d   ; r11 = v1
     xor r11, rcx                     ; r11 = v1 ^ key1
-
     mov r8, 0x06c7967656e657261		; r8 = v2
     xor r8, rdx                     ; r8 = key0 ^ v2
-
 	mov r9, 0x07465646279746573		; r9 = v3
     xor r9, rcx                    ; r9 = key1 ^ v3
 %endmacro
@@ -41,41 +38,27 @@ siphash_2_4:
 	; RCX key1 qword
 	
 ; INITIALIZATION
-	
 	InitMainValues
-
-	mov rdx, rsi							; sup[(rdx + 1) / 8] = rcx quotient, rdx remainder
-	inc rdx								
-	mov rcx, rdx
-	shl rcx, 3
-
-	mov r12, 1
-	mov rax, 0
-	and rdx, 7							; remainder rdx
-	cmovnz rax, r12
-	add rcx, rax						; rcx loop counter
-
-; COMPRESSION
-	dec rcx ; last cicle after the loop
+	mov rcx, rsi
+	shr rcx, 3
 	jz MessageLoadLoopEnd_2_4
 MessageLoadLoop_2_4:
 	mov rax, qword [rdi]
 	xor r9, rax
-	
 	SipRound
 	SipRound
-	
 	xor r10, rax
 	add rdi, 8
 	dec rcx
 	jnz MessageLoadLoop_2_4
-MessageLoadLoopEnd_2_4:
 
-	mov rax, 0
+MessageLoadLoopEnd_2_4:
+	xor rax, rax	; rax = 0
 	mov rdx, rsi
 	and rdx, 7		; rdx = mess_len % 8
 	je LastMessagePartEnd_2_4
 	add rdi, rdx
+
 LastMessagePart_2_4:
 	dec rdi
 	movzx rcx, byte [rdi]
@@ -83,28 +66,22 @@ LastMessagePart_2_4:
 	or rax, rcx
 	dec rdx
 	jnz LastMessagePart_2_4
+
 LastMessagePartEnd_2_4:
 	movzx rcx, sil
 	shl rcx, 56
 	or rax, rcx
-
 	xor r9, rax
-	
 	SipRound
 	SipRound
-
 	xor r10, rax
 
-
 ; FINALIZATION
-
 	xor r8, 0FFh
-
 	SipRound
 	SipRound
 	SipRound
 	SipRound
-
 	xor r8, r9
 	xor r8, r10
 	xor r8, r11
@@ -116,46 +93,31 @@ siphash_4_8:
 	; RSI mess_len qword unsigned
 	; R8 RDX key0 qword
 	; R9 RCX key1 qword
-	
 
 ; INITIALIZATION
-	
 	InitMainValues
-
-	mov rdx, rsi							; sup[(rdx + 1) / 8] = rcx quotient, rdx remainder
-	inc rdx								
-	mov rcx, rdx
-	shl rcx, 3
-
-	mov r12, 1
-	mov rax, 0
-	and rdx, 7							; remainder rdx
-	cmovnz rax, r12
-	add rcx, rax						; rcx loop counter
-
-; COMPRESSION
-	dec rcx ; last cicle after the loop
+	mov rcx, rsi
+	shr rcx, 3
 	jz MessageLoadLoopEnd_4_8
 MessageLoadLoop_4_8:
-	mov rax, qword[rdi]
+	mov rax, qword [rdi]
 	xor r9, rax
-	
 	SipRound
 	SipRound
 	SipRound
 	SipRound
-	
 	xor r10, rax
 	add rdi, 8
 	dec rcx
 	jnz MessageLoadLoop_4_8
-MessageLoadLoopEnd_4_8:
 
-	mov rax, 0
+MessageLoadLoopEnd_4_8:
+	xor rax, rax	; rax = 0
 	mov rdx, rsi
 	and rdx, 7		; rdx = mess_len % 8
 	je LastMessagePartEnd_4_8
 	add rdi, rdx
+
 LastMessagePart_4_8:
 	dec rdi
 	movzx rcx, byte [rdi]
@@ -163,36 +125,28 @@ LastMessagePart_4_8:
 	or rax, rcx
 	dec rdx
 	jnz LastMessagePart_4_8
+
 LastMessagePartEnd_4_8:
 	movzx rcx, sil
 	shl rcx, 56
 	or rax, rcx
-
 	xor r9, rax
-	
 	SipRound
 	SipRound
 	SipRound
 	SipRound
-
 	xor r10, rax
 
-
 ; FINALIZATION
-
 	xor r8, 0FFh
-
-
 	SipRound
 	SipRound
 	SipRound
 	SipRound
-
 	SipRound
 	SipRound
 	SipRound
 	SipRound
-
 	xor r8, r9
 	xor r8, r10
 	xor r8, r11
